3296286b581bdbc8e7164a2cee30b97b669ad7f6
Francisco Javier Jimenez Cohen <franjicohen@gmail.com>
2023-10-21 10:34:50 -0300

    feat/NXPC-0001 creacion de backend y estructura de carpetas para proyecto backend-aeroclub

---
+++ index.js
@@ -0,0 +1,4 @@
+import app from './src/server';
+import 'dotenv/config';
+
+app.listen()
---
+++ src/server/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+//middleware
+app.use(express.json());
+
+//routers
+
+
+
+export default app;
